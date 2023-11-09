daedbfa4eb37183f2bb643223327a799371b038c
Renan Pinho <pinhorenan@icloud.com>
2023-11-08 22:15:58 -0300

    Update SECRET_KEY handling in settings.py

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--um)$edums(0j7#w3x_o7umflx%b945cvl($ocz^5dj*(+dl1v' # noqa-E501
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY') # noqa-E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
